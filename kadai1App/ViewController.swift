19a22a26042722ff50a97519a096c83acf03ec30
akio0911 <akio0911@gmail.com>
2021-03-09 18:02:22 +0900

    feedback

--- kadai1App/ViewController.swift
+++ kadai1App/ViewController.swift
@@ -9,32 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var textField1: UITextField!
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var textField4: UITextField!
-    @IBOutlet weak var textField5: UITextField!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
     
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet private weak var resultLabel: UILabel!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
     @IBAction func pressButton(_ sender: Any) {
     
-        let num1: Int? = Int(textField1.text!) ?? 0
-        let num2: Int? = Int(textField2.text!) ?? 0
-        let num3: Int? = Int(textField3.text!) ?? 0
-        let num4: Int? = Int(textField4.text!) ?? 0
-        let num5: Int? = Int(textField5.text!) ?? 0
+        let num1: Int = Int(textField1.text!) ?? 0
+        let num2: Int = Int(textField2.text!) ?? 0
+        let num3: Int = Int(textField3.text!) ?? 0
+        let num4: Int = Int(textField4.text!) ?? 0
+        let num5: Int = Int(textField5.text!) ?? 0
     
-        let sum = Int(num1! + num2! + num3! + num4! + num5!)
+        let sum = Int(num1 + num2 + num3 + num4 + num5)
         resultLabel.text = String(sum)
-    
     }
-    
 }
-
